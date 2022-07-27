119f663cc35101b02045d33a16a161805ff9bb8f
Koji Hasegawa <hasegawa@hubsys.co.jp>
2022-07-28 05:07:22 +0900

    Add TimeoutAttribute using `yield return null` example

--- Assets/APIExamples/Tests/Runtime/NUnit/TimeoutAttributeExample.cs
+++ Assets/APIExamples/Tests/Runtime/NUnit/TimeoutAttributeExample.cs
@@ -1,7 +1,9 @@
-﻿// Copyright (c) 2021 Koji Hasegawa.
+﻿// Copyright (c) 2021-2022 Koji Hasegawa.
 // This software is released under the MIT License.
 
+using System;
 using System.Collections;
+using System.Diagnostics;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -14,11 +16,19 @@ namespace APIExamples.NUnit
     [TestFixture]
     public class TimeoutAttributeExample
     {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
         private const bool Fail = false; // このフラグをtrueにするとこのクラスのテストはすべて失敗します
+
+        [SetUp]
+        public void SetUp()
+        {
+            _stopwatch.Restart();
+        }
 
         [TearDown]
         public void TearDown()
         {
+            UnityEngine.Debug.Log($"{_stopwatch.ElapsedMilliseconds} [ms]");
             Time.timeScale = 1f;
         }
 
@@ -26,40 +36,57 @@ namespace APIExamples.NUnit
         [Explicit("時間がかかるのでExplicit")]
         public IEnumerator タイムアウトのデフォルトは3分()
         {
-            var waitSeconds = 3 * 60; // タイムアウトのデフォルトは3[min]
-            if (!Fail)
+            var waitSeconds = 3 * 60 - 1; // タイムアウトのデフォルトは3[min]
+            if (Fail)
             {
-                waitSeconds--;
+                waitSeconds++;
             }
 
             yield return new WaitForSeconds(waitSeconds);
         }
 
         [UnityTest]
-        [Timeout(1000)]
+        [Timeout(2000)]
         public IEnumerator タイムアウトを1秒に設定()
         {
             var waitSeconds = 1f;
-            if (!Fail)
+            if (Fail)
             {
-                waitSeconds -= 0.2f;
+                waitSeconds = 10f;
             }
 
             yield return new WaitForSeconds(waitSeconds);
         }
 
         [UnityTest]
-        [Timeout(1000)]
+        [Timeout(2000)]
         public IEnumerator タイムアウト値はTimeScaleに影響されない()
         {
-            var waitSeconds = 2f;
-            if (!Fail)
+            var waitSeconds = 1f;
+            if (Fail)
             {
-                waitSeconds -= 0.4f;
+                waitSeconds = 10f;
             }
 
-            Time.timeScale = 2f;
-            yield return new WaitForSeconds(waitSeconds);
+            Time.timeScale = 5f;
+            yield return new WaitForSecondsRealtime(waitSeconds);
+        }
+
+        [UnityTest]
+        [Timeout(2000)]
+        public IEnumerator タイムアウトはWaitForSeconds以外では有効でない_中断されない()
+        {
+            var waitSeconds = 1f;
+            if (Fail)
+            {
+                waitSeconds = 10f;
+            }
+
+            var startTime = DateTime.Now;
+            while ((DateTime.Now - startTime).Seconds < waitSeconds)
+            {
+                yield return null; // Timeout時間を超えてもテストは中断されない。指定時間を超過していれば失敗と判定はされる
+            }
         }
     }
 }
