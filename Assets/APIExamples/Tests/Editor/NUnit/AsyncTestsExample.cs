3fe93568052a0e820854d57c6bbccd2bd02592c9
Koji Hasegawa <hasegawa@hubsys.co.jp>
2022-10-29 03:32:12 +0900

    Add async tests
    https://docs.unity3d.com/Packages/com.unity.test-framework@1.3/manual/reference-async-tests.html

---
+++ Assets/APIExamples/Tests/Editor/NUnit/AsyncTestsExample.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) 2022 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace APIExamples.Editor.NUnit
+{
+    /// <summary>
+    /// 非同期テストの実装例（Edit Modeテスト）
+    /// Async tests example (in Edit Mode tests)
+    /// </summary>
+    /// <remarks>
+    /// Required: Unity Test Framework v1.3 or later
+    /// </remarks>
+    public class AsyncTestsExample
+    {
+        [Test]
+        [Description("Can await Task")]
+        public async Task 非同期テストの例_Taskをawaitできる()
+        {
+            await Foo(1);
+            var actual = await Bar(2);
+            Assert.That(actual, Is.EqualTo(3));
+        }
+
+        private static async Task Foo(int id)
+        {
+            Debug.Log($"Start Foo({id})");
+            await Task.Delay(200);
+            Debug.Log($"Exit Foo({id})");
+        }
+
+        private static async Task<int> Bar(int id)
+        {
+            Debug.Log($"Start Bar({id})");
+            await Task.Delay(200);
+            Debug.Log($"Exit Bar({id})");
+            return id + 1;
+        }
+
+        [Test]
+        [Description("Can await UniTask")]
+        public async Task 非同期テストの例_UniTaskをawaitできる()
+        {
+            await UniTaskFoo(1);
+            var actual = await UniTaskBar(2);
+            Assert.That(actual, Is.EqualTo(3));
+        }
+
+        private static async UniTask UniTaskFoo(int id)
+        {
+            Debug.Log($"Start UniTaskFoo({id})");
+            await UniTask.Delay(200);
+            Debug.Log($"Exit UniTaskFoo({id})");
+        }
+
+        private static async UniTask<int> UniTaskBar(int id)
+        {
+            Debug.Log($"Start UniTaskBar({id})");
+            await UniTask.Delay(200);
+            Debug.Log($"Exit UniTaskBar({id})");
+            return id + 1;
+        }
+
+        [Test]
+        [Description("Can await Coroutine")]
+        [Explicit("Edit Modeテストではフリーズするため実行対象から除外/ Freeze in the Edit Mode tests")]
+        public async Task 非同期テストの例_コルーチンをawaitできる()
+        {
+            var actual = 0;
+            await FooMonoBehaviour.BarCoroutine(i =>
+            {
+                actual = i;
+            });
+
+            Assert.That(actual, Is.EqualTo(1));
+        }
+
+        // テスト対象コルーチンを含むMonoBehaviour
+        private class FooMonoBehaviour : MonoBehaviour
+        {
+            public static IEnumerator BarCoroutine(Action<int> onSuccess)
+            {
+                yield return null;
+                onSuccess(1);
+            }
+        }
+    }
+}
---
+++ Assets/APIExamples/Tests/Runtime/NUnit/AsyncSetupAttributeExample.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2022 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace APIExamples.NUnit
+{
+    /// <summary>
+    /// 非同期の<see cref="SetUpAttribute"/>, <see cref="TearDownAttribute"/>の例
+    /// Async SetUp and TearDown example
+    /// </summary>
+    /// <remarks>
+    /// Required: Unity Test Framework v1.3 or later
+    /// <see cref="OneTimeSetUpAttribute"/>, <see cref="OneTimeTearDownAttribute"/>はasyncサポートされていない
+    /// </remarks>
+    [UnityPlatform(exclude = new[] { RuntimePlatform.WebGLPlayer })]
+    public class AsyncSetupAttributeExample
+    {
+        /// <summary>
+        /// 各テストメソッドの前に実行されます
+        /// </summary>
+        [SetUp]
+        public async Task SetUp()
+        {
+            Debug.Log($"SetUp, {Time.time}");
+            await Task.Delay(200);
+        }
+
+        /// <summary>
+        /// 各テストメソッドの前に実行されます
+        /// </summary>
+        [TearDown]
+        public async Task TearDown()
+        {
+            Debug.Log($"TearDown, {Time.time}");
+            await Task.Delay(200);
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            Debug.Log($"TestMethod, {Time.time}");
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Debug.Log($"TestMethod2, {Time.time}");
+        }
+    }
+}
