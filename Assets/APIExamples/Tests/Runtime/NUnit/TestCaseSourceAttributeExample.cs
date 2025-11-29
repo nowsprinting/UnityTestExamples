6cd7398842ba9edfd61caca20de64fab7c8e1d5e
Koji Hasegawa <hasegawa@hubsys.co.jp>
2025-11-29 17:29:16 +0900

    Add TestCaseData with TestName

--- Assets/APIExamples/Tests/Runtime/NUnit/TestCaseSourceAttributeExample.cs
+++ Assets/APIExamples/Tests/Runtime/NUnit/TestCaseSourceAttributeExample.cs
@@ -1,30 +1,28 @@
 ﻿// Copyright (c) 2021-2025 Koji Hasegawa.
 // This software is released under the MIT License.
 
-using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using UnityEngine.TestTools;
-
-// ReSharper disable AccessToStaticMemberViaDerivedType
 
 namespace APIExamples.NUnit
 {
     /// <summary>
     /// <see cref="TestCaseSourceAttribute"/> によるパラメタライズドテストの記述例.
     /// <p/>
-    /// <see cref="TestCaseSourceAttribute"/> は、入力要素と期待値の組み合わせを <c>static</c> <see cref="IEnumerable"/> で指定できます。
     /// <see cref="UnityTestAttribute"/> と組み合わせては使用できません。
     /// </summary>
     /// <remarks>
     /// 1xダメージのテストケースは <see cref="ValueSourceAttributeExample"/> を参照してください。
     /// </remarks>
+    /// <seealso cref="TestCaseAttributeExample"/>
     [TestFixture]
+    [SuppressMessage("ReSharper", "AccessToStaticMemberViaDerivedType")]
     public class TestCaseSourceAttributeExample
     {
         // テストケースを静的フィールドで定義
-        // Note: TestCaseData は NUnit でなく、Unity Test Framework の提供するデータ型です
         private static TestCaseData[] s_weaknessElementCombinationCases =
         {
             new TestCaseData(Element.Fire, Element.Water), // 火 ← 水
@@ -32,16 +30,14 @@ namespace APIExamples.NUnit
             new TestCaseData(Element.Wood, Element.Fire),  // 木 ← 火
         };
 
-        [TestCaseSource(nameof(s_weaknessElementCombinationCases))] // フィールドを指定する例
+        [TestCaseSource(nameof(s_weaknessElementCombinationCases))]
         public void GetDamageMultiplier_弱点属性からの攻撃はダメージ2x(Element defence, Element attack)
         {
             var actual = defence.GetDamageMultiplier(attack);
-
             Assert.That(actual, Is.EqualTo(2.0f));
         }
 
         // テストケースを返す静的メソッド
-        // Note: TestCaseData は NUnit でなく、Unity Test Framework の提供するデータ型です
         public static IEnumerable<TestCaseData> GetWeaknessElementCombinationCases()
         {
             yield return new TestCaseData(Element.Fire, Element.Water); // 火 ← 水
@@ -49,11 +45,10 @@ namespace APIExamples.NUnit
             yield return new TestCaseData(Element.Wood, Element.Fire);  // 木 ← 火
         }
 
-        [TestCaseSource(nameof(GetWeaknessElementCombinationCases))] // メソッドを指定する例
+        [TestCaseSource(nameof(GetWeaknessElementCombinationCases))]
         public void GetDamageMultiplier_弱点属性からの攻撃はダメージ2x_メソッド指定の例(Element defence, Element attack)
         {
             var actual = defence.GetDamageMultiplier(attack);
-
             Assert.That(actual, Is.EqualTo(2.0f));
         }
 
@@ -69,15 +64,29 @@ namespace APIExamples.NUnit
         }
 
         [TestCaseSource(typeof(TestCasesInClass), nameof(TestCasesInClass.GetWeaknessElementCombinationCasesInClass))]
-        public void GetDamageMultiplier_弱点属性からの攻撃はダメージ2x_別クラスの例(Element defence, Element attack)
+        public void GetDamageMultiplier_弱点属性からの攻撃はダメージ2x_クラス指定の例(Element defence, Element attack)
         {
             var actual = defence.GetDamageMultiplier(attack);
+            Assert.That(actual, Is.EqualTo(2.0f));
+        }
 
+        // 名前付きTestCaseData
+        private static TestCaseData[] s_weaknessElementCombinationCasesWithName =
+        {
+            new TestCaseData(Element.Fire, Element.Water).SetName("火 ← 水"),
+            new TestCaseData(Element.Water, Element.Wood).SetName("水 ← 木"),
+            new TestCaseData(Element.Wood, Element.Fire).SetName("木 ← 火"),
+        };
+
+        [TestCaseSource(nameof(s_weaknessElementCombinationCasesWithName))]
+        public void TestCaseDataにTestNameプロパティを指定した例(Element defence, Element attack)
+        {
+            var actual = defence.GetDamageMultiplier(attack);
             Assert.That(actual, Is.EqualTo(2.0f));
         }
 
         [TestCaseSource(nameof(s_weaknessElementCombinationCases))]
-        public async Task 非同期テストではTestCaseSource属性を使用できる(Element defence, Element attack)
+        public async Task 非同期テストでもTestCaseSource属性を使用できる(Element defence, Element attack)
         {
             var actual = defence.GetDamageMultiplier(attack);
             await Task.Yield();
--- Assets/APIExamples/Tests/Runtime/NUnit/ValueSourceAttributeExample.cs
+++ Assets/APIExamples/Tests/Runtime/NUnit/ValueSourceAttributeExample.cs
@@ -3,27 +3,26 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using UnityEngine.TestTools;
-
-// ReSharper disable AccessToStaticMemberViaDerivedType
 
 namespace APIExamples.NUnit
 {
     /// <summary>
     /// <see cref="ValueSourceAttribute"/> によるパラメタライズドテストの記述例.
     /// <p/>
-    /// <see cref="ValueSourceAttribute"/> は組み合わせテストになるため、期待値が同じになる要素ごとにテストメソッドを定義するとわかりやすくなります。
-    /// <p/>
-    /// 組み合わせ生成ルールのデフォルトは全網羅です。明示的に全網羅であることを示すには <see cref="CombinatorialAttribute"/> を使用できます。
+    /// 組み合わせ生成ルールのデフォルトは全組み合わせ網羅です。明示的に全組み合わせ網羅であることを示すには <see cref="CombinatorialAttribute"/> を使用できます。
     /// 組み合わせを絞り込む <see cref="PairwiseAttribute"/> のサンプルは <see cref="PairwiseAttributeExample"/> を参照してください。
     /// 組み合わせを固定する <see cref="SequentialAttribute"/> のサンプルは <see cref="SequentialAttributeExample"/> を参照してください。
     /// </summary>
     /// <remarks>
     /// 2xダメージのテストケースは <see cref="TestCaseSourceAttributeExample"/> を参照してください。
     /// </remarks>
+    /// <seealso cref="ValuesAttributeExample"/>
     [TestFixture]
+    [SuppressMessage("ReSharper", "AccessToStaticMemberViaDerivedType")]
     public class ValueSourceAttributeExample
     {
         // テストケースの引数が取る値を静的フィールドで定義
@@ -31,15 +30,15 @@ namespace APIExamples.NUnit
 
         [Test]
         public void GetDamageMultiplier_無属性からの攻撃はすべて等倍ダメージ(
-            [ValueSource(nameof(s_elements))] Element defence) // フィールドを指定する例
+            [ValueSource(nameof(s_elements))] Element defence, // フィールドを指定する例
+            [Values(Element.None)] Element attack)
         {
-            var actual = defence.GetDamageMultiplier(Element.None);
-
+            var actual = defence.GetDamageMultiplier(attack);
             Assert.That(actual, Is.EqualTo(1.0f));
         }
 
-        // テストケースの引数が取る値を返す静的メソッド
-        public static IEnumerable<Element> GetElements()
+        // テストケースの引数が取る値を静的メソッドで定義
+        private static IEnumerable<Element> GetElements()
         {
             yield return Element.None;
             yield return Element.Fire;
@@ -49,10 +48,10 @@ namespace APIExamples.NUnit
 
         [Test]
         public void GetDamageMultiplier_無属性への攻撃はすべて等倍ダメージ(
+            [Values(Element.None)] Element defence,
             [ValueSource(nameof(GetElements))] Element attack) // メソッドを指定する例
         {
-            var actual = Element.None.GetDamageMultiplier(attack);
-
+            var actual = defence.GetDamageMultiplier(attack);
             Assert.That(actual, Is.EqualTo(1.0f));
         }
 
@@ -61,12 +60,11 @@ namespace APIExamples.NUnit
         [ParametrizedIgnore(Element.Water, Element.Wood)]
         [ParametrizedIgnore(Element.Wood, Element.Fire)]
         public void GetDamageMultiplier_ParametrizedIgnore属性で等倍ダメージにならない組み合わせを除外_すべて等倍ダメージ(
-            // Note: ParametrizedIgnore 属性は NUnit でなく、Unity Test Framework v1.4 で追加された属性です
+            // Note: ParametrizedIgnore 属性は Unity Test Framework v1.4 で追加された属性です
             [ValueSource(nameof(s_elements))] Element defence,
-            [ValueSource(nameof(s_elements))] Element attack)
+            [ValueSource(nameof(GetElements))] Element attack)
         {
             var actual = defence.GetDamageMultiplier(attack);
-
             Assert.That(actual, Is.EqualTo(1.0f));
         }
 
@@ -77,7 +75,7 @@ namespace APIExamples.NUnit
         public IEnumerator UnityTestでもValueSource属性は使用可能(
             // Note: UnityTest で ParametrizedIgnore 属性が使用できるのは、Unity Test Framework v1.4.6以降です
             [ValueSource(nameof(s_elements))] Element defence,
-            [ValueSource(nameof(s_elements))] Element attack)
+            [ValueSource(nameof(GetElements))] Element attack)
         {
             var actual = defence.GetDamageMultiplier(attack);
             yield return null;
@@ -92,7 +90,7 @@ namespace APIExamples.NUnit
         public async Task 非同期テストでもValueSource属性は使用可能(
             // Note: Async test で ParametrizedIgnore 属性が使用できるのは、Unity Test Framework v1.4.6以降です
             [ValueSource(nameof(s_elements))] Element defence,
-            [ValueSource(nameof(s_elements))] Element attack)
+            [ValueSource(nameof(GetElements))] Element attack)
         {
             var actual = defence.GetDamageMultiplier(attack);
             await Task.Yield();
